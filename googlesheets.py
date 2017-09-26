a5154a3d54c439a74fce0eab87968a99d3e00f27
Zeroxk <dhu009@student.uib.no>
2017-09-26 23:54:15 +0200

    Inital commit

---
+++ googlesheets.py
@@ -0,0 +1,76 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'GW matchscore tracker'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gw.matchscore.tracker.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def write_to_sheet(myScore, oppScore, current_time, usOnline, oppOnline):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                            discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = '1N8KaYe7a3VLcbPz3VLwdMeUXcq5GR3fLlqPGj4NbMoE'
+    range_name = 'Day2!A:C'
+
+    values = [
+        [
+            current_time, myScore, oppScore
+        ]
+    ]
+    
+    body = {
+        'values': values
+    }
+
+    result = service.spreadsheets().values().append(
+        spreadsheetId=spreadsheetId, range=range_name,
+        valueInputOption='USER_ENTERED', body=body).execute()
+
+def main():
+    write_to_sheet(100, 99, '01:02:03', 3, 4)
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+from time import sleep
+import googlesheets
+import pytz
+
+def findRecentlyActivePlayers(guildID):
+    driver.get(GBF_URL + '#guild/detail/{}'.format(guildID) )
+    elements = driver.find_elements_by_class_name('prt-status-container')
+    val = elements[-1].find_element_by_class_name('prt-status-value').text
+    print('val: {}'.format(val))
+    return int(val)
+
+def main():
+
+    '''Setup'''
+    options = webdriver.ChromeOptions()
+    global driver
+    global GBF_URL
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.implicitly_wait(5)
+    GBF_URL = 'http://game.granbluefantasy.jp/'
+    GW_HOME_URL = GBF_URL + '#event/teamraid033'
+    myGuildID = '147448'
+    oppGuildID = '654678'
+    driver.get(GBF_URL)
+
+    '''user logs in'''
+    input('Press enter to continue')
+
+    driver.get(GW_HOME_URL)
+
+    jst = pytz.timezone('Asia/Tokyo')
+    start = datetime.now(jst).replace(hour=7, minute=0, second=0)
+    end = datetime.now(jst).replace(hour=0, minute=0, second=0)
+    
+    now = datetime.now(jst)
+    
+    while now.hour > end.hour and now.hour >= start.hour:
+        myScore = driver.find_element_by_class_name('txt-guild-point').text.replace(',','')
+        oppScore = driver.find_element_by_class_name('txt-rival-point').text.replace(',','')
+
+        usOnline = 0
+        oppOnline = 0
+
+        print('myScore: {}\toppScore: {}\tusOnline: {}\toppOnline: {}'.format(myScore,oppScore,usOnline,oppOnline))
+        
+        print('Current time: {}'.format(now.time()) )
+        googlesheets.write_to_sheet(int(myScore), int(oppScore), now.strftime('%H:%M:%S'), usOnline, oppOnline)
+
+        refreshInterval = 600
+        sleep(refreshInterval)
+        driver.refresh()
+        now = datetime.now(jst)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception:
+        print(Exception)
+        raise
