db4bd4b2ad4785da8b60772749b135fddc25f358
Zeroxk <dhu009@student.uib.no>
2017-11-12 21:27:09 +0100

    Supports config file, fixed findRecentlyActivePlayers.

--- main.py
+++ main.py
@@ -1,55 +1,84 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 from datetime import datetime
 from time import sleep
 import googlesheets
 import pytz
+import json
 
 def findRecentlyActivePlayers(guildID):
-    driver.get(GBF_URL + '#guild/detail/{}'.format(guildID) )
-    elements = driver.find_elements_by_class_name('prt-status-container')
-    val = elements[-1].find_element_by_class_name('prt-status-value').text
-    print('val: {}'.format(val))
-    return int(val)
 
+    try:
+        driver.get('{}#guild/detail/{}'.format(GBF_URL,guildID) )
+        elements = WebDriverWait(driver, 10).until(
+            expected_conditions.visibility_of_all_elements_located((By.CLASS_NAME,'prt-status-container')) 
+        )
+        val = elements[-1].find_element_by_class_name('prt-status-value').text
+
+        return int(val)
+
+    except Exception as err:
+        print("Error when trying to find recently active players {}".format(err))
+    
 def main():
 
     '''Setup'''
+    with open('config.json') as f:
+        config = json.load(f)
+
     options = webdriver.ChromeOptions()
+    profile_dir = config['profile_dir']
+    if profile_dir != '':
+        options.add_argument('user-data-dir={}'.format(profile_dir))
+
     global driver
     global GBF_URL
     driver = webdriver.Chrome(chrome_options=options)
     driver.implicitly_wait(5)
-    GBF_URL = 'http://game.granbluefantasy.jp/'
-    GW_HOME_URL = GBF_URL + '#event/teamraid033'
-    myGuildID = '147448'
-    oppGuildID = '137217'
+    GBF_URL = config['gbf_url']
+    GW_HOME_URL = GBF_URL + config['gw_partial_url']
+    myGuildID = config['myGuildID']
+    oppGuildID = config['oppGuildID']
     driver.get(GBF_URL)
 
     '''user logs in'''
-    input('Press enter to continue')
-
+    if profile_dir == '':
+        input('Press enter to continue')
+    
     driver.get(GW_HOME_URL)
 
     jst = pytz.timezone('Asia/Tokyo')
     start = datetime.now(jst).replace(hour=7, minute=0, second=0)
     end = datetime.now(jst).replace(hour=0, minute=0, second=0)
     
+    googlesheets.setup(config['spreadsheetID'])
+
     now = datetime.now(jst)
-    refreshInterval = 600
-    
+    refreshInterval = config['refresh_interval']
+
     while now.hour > end.hour and now.hour >= start.hour:
         myScore = driver.find_element_by_class_name('txt-guild-point').text.replace(',','')
         oppScore = driver.find_element_by_class_name('txt-rival-point').text.replace(',','')
 
-        usOnline = 0
-        oppOnline = 0
+        usOnline = findRecentlyActivePlayers(myGuildID)
+        oppOnline = findRecentlyActivePlayers(oppGuildID)
 
         print('myScore: {}\toppScore: {}\tusOnline: {}\toppOnline: {}'.format(myScore,oppScore,usOnline,oppOnline))
         
         print('Current time: {}'.format(now.time()) )
-        googlesheets.write_to_sheet(int(myScore), int(oppScore), now.strftime('%H:%M:%S'), usOnline, oppOnline)
 
+        values = [
+            [
+                now.strftime('%H:%M:%S'), int(myScore), int(oppScore), usOnline, oppOnline
+            ]
+        ]
+
+        googlesheets.write_to_sheet(values, config['sheet_range_name'])
+
+        driver.get(GW_HOME_URL)
         sleep(refreshInterval)
         now = datetime.now(jst)
         driver.refresh()
